711867b7bd868878101f2f6e2515989eadaa923b
Aditya <95631458+Sharmaaditya05@users.noreply.github.com>
2023-05-11 22:07:39 +0530

    ..

--- src/Component/Pizza.js
+++ src/Component/Pizza.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react'
 
+import React, { useState } from 'react'
+import { Modal } from 'react-bootstrap'
+import { alignPropType } from 'react-bootstrap/esm/types';
 export default function Pizza({ pizza }) {
   const [quantity, setquantity] = useState(1)
   const [varient, setvarient] = useState('small')
   // here we take taken the two variables for the prices and total amount
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div style={{margin:"60px"}} className='shadow p-3 mb-5 bg-white rounded'>
-      <h1>{pizza.name}</h1>
-      <img src={pizza.image} className="img-fluid" style={{ height: '200px', width: '200px' }} />
+    <div style={{ margin: "60px" }} className='shadow p-3 mb-5 bg-white rounded'>
+      <div onClick={handleShow} >
+        <h1>{pizza.name}</h1>
+        <img src={pizza.image} className="img-fluid" style={{ height: '200px', width: '200px' }} />
+      </div>
+
       {/* in using style wew use inline styling/css */}
       <div className='flex-container'>
         <div className='w-100 m-1'>
@@ -21,7 +31,7 @@ export default function Pizza({ pizza }) {
         </div>
 
         <div className='w-100 m-1'>
-          <p>Quantity</p>
+          <p>Quantity </p>
           <select className='form-control' value={quantity} onChange={(e) => { setquantity(e.target.value) }}>
             {[...Array(10).keys()].map((x, i) => {
               return <option value={i + 1}>{i + 1}</option>
@@ -39,8 +49,22 @@ export default function Pizza({ pizza }) {
         <div className='m-1 w-100'>
           <button className='btn'>Add to Cart</button>
         </div >
-
       </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <img src={pizza.image} className='img-fluid' style={{height:"300px" }}/>
+          <p>{pizza.description}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className='btn' onClick={handleClose}>Close</button>
+        </Modal.Footer>
+      </Modal>
+
 
     </div>
   )
---
+++ src/pizzasdata.js
@@ -0,0 +1,184 @@
+const pizzas = [
+    {
+        name: "Golden Corn",
+
+        varients: [
+
+            "small",
+
+            "medium",
+
+            "large"
+
+        ],
+
+        prices: [
+
+            {
+
+                small: 200,
+
+                medium: 350,
+
+                large: 400
+
+            }
+        ],
+
+        category: "veg",
+
+        image: "https://media.istockphoto.com/id/938742222/photo/cheesy-pepperoni-pizza.jpg?s=612x612&w=is&k=20&c=cjrsXpA0waA1Ee50_aVdrY-FCpREkEuf0wkJmm20dAc=",
+        description: "Pepper Barbecue Chicken I Cheese"
+
+    }, {
+
+        name: "Veg mar",
+        varients: [
+
+            "small",
+
+            "medium",
+
+            "large"
+
+        ],
+
+        prices: [
+
+            {
+
+                small: 200,
+
+                medium: 350,
+
+                large: 400
+
+            }
+        ],
+
+        category: "veg",
+
+        image: "https://media.istockphoto.com/id/938742222/photo/cheesy-pepperoni-pizza.jpg?s=612x612&w=is&k=20&c=cjrsXpA0waA1Ee50_aVdrY-FCpREkEuf0wkJmm20dAc=",
+        description: "Pepper Barbecue Chicken I Cheese"
+
+    }, {
+        name: "Non Veg Supreme",
+        varients: [
+
+            "small",
+
+            "medium",
+
+            "large"
+
+        ],
+
+        prices: [
+
+            {
+
+                small: 200,
+
+                medium: 350,
+
+                large: 400
+
+            }
+        ],
+        category: "Non veg",
+
+        image: "https://media.istockphoto.com/id/938742222/photo/cheesy-pepperoni-pizza.jpg?s=612x612&w=is&k=20&c=cjrsXpA0waA1Ee50_aVdrY-FCpREkEuf0wkJmm20dAc=",
+        description: "Pepper Barbecue Chicken I Cheese"
+
+    }, {
+
+        name: "Veg Manvi fav.",
+        varients: [
+
+            "small",
+
+            "medium",
+
+            "large"
+
+        ],
+
+        prices: [
+
+            {
+
+                small: 200,
+
+                medium: 350,
+
+                large: 400
+
+            }
+        ],
+
+        category: "veg",
+
+        image: "https://media.istockphoto.com/id/938742222/photo/cheesy-pepperoni-pizza.jpg?s=612x612&w=is&k=20&c=cjrsXpA0waA1Ee50_aVdrY-FCpREkEuf0wkJmm20dAc=",
+        description: "Pepper Barbecue Chicken I Cheese"
+
+    }, {
+        name: "Non Veg Manvi fav.",
+        varients: [
+
+            "small",
+
+            "medium",
+
+            "large"
+
+        ],
+
+        prices: [
+
+            {
+
+                small: 200,
+
+                medium: 350,
+
+                large: 400
+
+            }
+        ],
+        category: "Non veg",
+
+        image: "https://media.istockphoto.com/id/938742222/photo/cheesy-pepperoni-pizza.jpg?s=612x612&w=is&k=20&c=cjrsXpA0waA1Ee50_aVdrY-FCpREkEuf0wkJmm20dAc=",
+        description: "Pepper Barbecue Chicken I Cheese"
+
+    }, {
+        name: "Veg Supreme",
+        varients: [
+
+            "small",
+
+            "medium",
+
+            "large"
+
+        ],
+
+        prices: [
+
+            {
+
+                small: 200,
+
+                medium: 350,
+
+                large: 400
+
+            }
+        ],
+
+        category: "veg",
+
+        image: "https://media.istockphoto.com/id/938742222/photo/cheesy-pepperoni-pizza.jpg?s=612x612&w=is&k=20&c=cjrsXpA0waA1Ee50_aVdrY-FCpREkEuf0wkJmm20dAc=",
+        description: "Pepper Barbecue Chicken I Cheese"
+
+    }]
+export default pizzas
